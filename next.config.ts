074b9c35b7f5c5b0a5696958e92a2b607d0abc8f
ahmed <ahmxd989@gmail.com>
2025-03-20 23:35:46 +0400

    updated

--- next.config.ts
+++ next.config.ts
@@ -4,13 +4,5 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
 };
-module.exports = {
-  env: {
-    LEETCODE_PLAYLIST_ID: process.env.LEETCODE_PLAYLIST_ID,
-    CODEFORCES_PLAYLIST_ID: process.env.CODEFORCES_PLAYLIST_ID,
-    CODECHEF_PLAYLIST_ID: process.env.CODECHEF_PLAYLIST_ID,
-    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
-  },
-};
 
 export default nextConfig;
